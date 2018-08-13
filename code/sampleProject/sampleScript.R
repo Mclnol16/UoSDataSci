806faae1ed28c61cb686ecec010b80c1e62082c1
Jason M Graham <prof.jason.m.graham@gmail.com>
2018-08-13 12:40:55 -0400

    added a sample RStudio project

---
+++ code/sampleProject/sampleScript.R
@@ -0,0 +1,17 @@
+# Name
+# Date
+# Description
+
+# import libraries
+library(tidyverse)
+
+# load some data and assign to variable df
+df <- read_csv("Data/sampleData.csv")
+
+# look at first five rows
+head(df,5)
+
+# look at last five rows
+tail(df,5)
+
+
